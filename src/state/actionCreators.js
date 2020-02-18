b98520262ec7f0f0aca5f17da6d8691c37201f9d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-18 11:03:51 +0100

    wip

--- src/state/actionCreators.js
+++ src/state/actionCreators.js
@@ -2,8 +2,14 @@ import * as types from './actionTypes'
 import uuid from 'uuid'
 
 // STEP-7 MAKE ACTION CREATORS THE COMPONENTS CAN USE DIRECTLY
-export function changeInput() {
-
+export function changeInput({ inputName, inputValue }) {
+  return { // this is an action. What should it have?
+    type: types.INPUT_CHANGE,
+    payload: {
+      inputName,
+      inputValue,
+    }
+  }
 }
 
 export function addFriend() {
